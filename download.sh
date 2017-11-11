6329ab63aca8feeaf040ce217abd45ec57112b84
Honglin Ju <honglinj@dataminer.informatik.tu-muenchen.de>
2017-11-11 12:11:48 +0100

    add download.sh

---
+++ download.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for ((i=2010;i<=2014;i++)) do
+wget "https://wwwdasis.samhsa.gov/dasis2/teds_pubs/TEDS/Admissions/${i}/TEDSA_${i}_PUF_CSV.zip"
+unzip TEDSA_${i}_PUF_CSV.zip
+done
+
+for ((i=2010;i<=2014;i++)) do
+wget "https://wwwdasis.samhsa.gov/dasis2/teds_pubs/TEDS/Discharges/TED_D_${i}/teds_d_${i}_csv.zip"
+unzip teds_d_${i}_csv.zip
+done
+
+for i in 2010 2012 2014
+do
+wget "https://wwwdasis.samhsa.gov/dasis2/nmhss/mh${i}_puf_csv.zip"
+unzip mh${i}_puf_csv.zip
+done
+~
