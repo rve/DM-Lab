423fceeed4d56a23f064ff16d395fee3f95b0daa
honglin <h.ju@buildingradar.com>
2017-11-22 09:52:19 +0100

    week2

--- week2/vis.R
+++ week2/vis.R
@@ -1,97 +1,16 @@
-# general visualisation
-library('ggplot2') # visualisation
-library('scales') # visualisation
-library('grid') # visualisation
-library('gridExtra') # visualisation
-library('RColorBrewer') # visualisation
-library('corrplot') # visualisation
-
-# general data manipulation
-library('dplyr') # data manipulation
-library('readr') # input/output
-library('data.table') # data manipulation
-library('tibble') # data wrangling
-library('tidyr') # data wrangling
-library('stringr') # string manipulation
-library('forcats') # factor manipulation
-library('magrittr')
-
-# Dates
-library('lubridate') # date and time
-
-# Extra vis
-library('ggforce') # visualisation
-library('ggridges') # visualisation
-
-
-# Define multiple plot function
-#
-# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
-# - cols:   Number of columns in layout
-# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
-#
-# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
-# then plot 1 will go in the upper left, 2 will go in the upper right, and
-# 3 will go all the way across the bottom.
-#
-multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
-  
-  # Make a list from the ... arguments and plotlist
-  plots <- c(list(...), plotlist)
-  
-  numPlots = length(plots)
-  
-  # If layout is NULL, then use 'cols' to determine layout
-  if (is.null(layout)) {
-    # Make the panel
-    # ncol: Number of columns of plots
-    # nrow: Number of rows needed, calculated from # of cols
-    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-                     ncol = cols, nrow = ceiling(numPlots/cols))
-  }
-  
-  if (numPlots==1) {
-    print(plots[[1]])
-    
-  } else {
-    # Set up the page
-    grid.newpage()
-    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-    
-    # Make each plot, in the correct location
-    for (i in 1:numPlots) {
-      # Get the i,j matrix positions of the regions that contain this subplot
-      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-      
-      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
-                                      layout.pos.col = matchidx$col))
-    }
-  }
-}
-
-# function to extract binomial confidence levels
-get_binCI <- function(x,n) as.list(setNames(binom.test(x,n)$conf.int, c("lwr", "upr")))
-
-train <- as.tibble(fread('./final.csv'))
-getwd()
-summary(train)
-glimpse(train)
-sum(is.na(train))
-
-
-
-train <- train %>%
-  mutate(state = factor(STFIPS),
-         #gender = factor(gender),
-         daywait = factor(DAYWAIT),
-         year = factor(YEAR))
-
+su
 p1 <- train %>%
-  ggplot(aes(GENDER>0, fill = GENDER)) +
+  ggplot(aes(DAYWAIT, fill = DAYWAIT)) +
   geom_bar() +
   theme(legend.position = "none")
+                    
+p2 <- train %>%
+  ggplot(aes(GENDER, fill = GENDER)) +
+  geom_bar() +
+  theme(legend.position = "none")
+
 layout <- matrix(c(1,1,2,2,3,3,4,4,4,5,5,5),2,6,byrow=TRUE)
-multiplot(p1, p2, p3, p4, p5, layout=layout)
+multiplot(p1, p2, layout=layout)
 
 p1 <- train %>%
   ggplot(aes(bd, reorder(reg_via, -bd, FUN = median), fill = reg_via)) +
@@ -121,3 +40,24 @@ p3 <- train  %>%
 
 layout <- matrix(c(1,2,1,2,1,2,3,3,3,3),5,2,byrow=TRUE)
 multiplot(p1, p2, p3, layout=layout)
+
+
+
+sum(train$DAYWAIT < 0); percent(sum(train$DAYWAIT < 0) / sum(train$DAYWAIT >= -100))
+sum(train$GENDER < 0); percent(sum(train$GENDER < 0) / sum(train$GENDER >= -100))
+sum(train$RACE < 0); percent(sum(train$RACE < 0) / sum(train$RACE >= -100))
+sum(train$ETHNIC < 0); percent(sum(train$ETHNIC < 0) / sum(train$ETHNIC >= -100))
+sum(train$MARSTAT < 0); percent(sum(train$MARSTAT < 0) / sum(train$MARSTAT >= -100))
+sum(train$PREG < 0); percent(sum(train$PREG < 0) / sum(train$PREG >= -100))
+
+sum(train$SUB1 < 0); percent(sum(train$SUB1 < 0) / sum(train$SUB1 >= -100))
+sum(train$VET < 0); percent(sum(train$VET < 0) / sum(train$VET >= -100))
+sum(train$EMPLOY < 0); percent(sum(train$EMPLOY < 0) / sum(train$EMPLOY >= -100))
+sum(train$EDUC < 0); percent(sum(train$EDUC < 0) / sum(train$EDUC >= -100))
+
+
+# library('plyr')
+# fun <- function(x){
+#   percent(sum(trainx< 0) / sum(x >= -100))
+# }   
+# colwise(fun)(train)
