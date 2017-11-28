23868d7b18bf3db7789cc194fb09f9e980283831
honglin <h.ju@buildingradar.com>
2017-11-28 15:34:08 +0100

    update download.sh

--- utils/download.sh
+++ utils/download.sh
@@ -9,10 +9,10 @@ done
 #for ((i=2010;i<=2014;i++)) do
 #wget "https://wwwdasis.samhsa.gov/dasis2/teds_pubs/TEDS/Discharges/TED_D_${i}/teds_d_${i}_csv.zip"
 
-for i in 2010 2012 2014
-do
-wget "https://wwwdasis.samhsa.gov/dasis2/nmhss/mh${i}_puf_csv.zip"
-unzip mh${i}_puf_csv.zip
-rm mh${i}_puf_csv.zip
-done
+#for i in 2010 2012 2014
+#do
+#wget "https://wwwdasis.samhsa.gov/dasis2/nmhss/mh${i}_puf_csv.zip"
+#unzip mh${i}_puf_csv.zip
+#rm mh${i}_puf_csv.zip
+#done
 
